c71dd777e97a66cddcf71b1c4d4d8d17747e447d
samleivadev <samdleiva@gmail.com>
2023-03-13 00:50:37 -0400

    topbar completed

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,24 +1,30 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 
+import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
+import { useState } from "react";
 
 function App() {
   const [theme, colorMode] = useMode();
-  
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline/>
-    <div className="app">
-      <main className="content">
-        <Topbar/>
-      </main>
-     
-    </div>  
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              
+            </Routes>
+          </main>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
-    
   );
 }
 
---
+++ react-admin/src/scenes/global/Sidebar.jsx
@@ -0,0 +1,59 @@
+//import "react-pro-sidebar/dist/css/styles.css";
+
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Menu, MenuItem, ProSidebar } from "react-pro-sidebar";
+
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import { useState } from "react";
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      {/*  */}
+    </Box>
+  );
+};
+
+export default Sidebar;
